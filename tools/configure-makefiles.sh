f64b96f68aba2f9566e8932358cd06a661b2516f
kobalicek <kobalicek.petr@gmail.com>
2024-07-31 11:08:52 +0200

    [build] Pass extra parameters to tools/configure script

--- tools/configure-makefiles.sh
+++ tools/configure-makefiles.sh
@@ -7,9 +7,9 @@ if [ -n "$VCPKG_ROOT" ]; then
 fi
 
 echo "== [Configuring Build - Debug] =="
-eval cmake .. -B ../build/Debug -DCMAKE_BUILD_TYPE=Debug ${BUILD_OPTIONS}
+eval cmake .. -B ../build/Debug -DCMAKE_BUILD_TYPE=Debug ${BUILD_OPTIONS} "$@"
 echo ""
 
 echo "== [Configuring Build - Release] =="
-eval cmake .. -B ../build/Release -DCMAKE_BUILD_TYPE=Release ${BUILD_OPTIONS}
+eval cmake .. -B ../build/Release -DCMAKE_BUILD_TYPE=Release ${BUILD_OPTIONS} "$@"
 echo ""
--- tools/configure-xcode.sh
+++ tools/configure-xcode.sh
@@ -6,4 +6,4 @@ if [ -n "$VCPKG_ROOT" ]; then
   BUILD_OPTIONS="${BUILD_OPTIONS} -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
 fi
 
-cmake .. -B ../build_xcode -G"Xcode" ${BUILD_OPTIONS}
+cmake .. -B ../build_xcode -G"Xcode" ${BUILD_OPTIONS} "$@"
